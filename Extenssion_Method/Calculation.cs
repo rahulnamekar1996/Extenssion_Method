55d34f2f491fde2f5e02cafb23fbd1f9565b9788
Owner <Owner@DESKTOP-2TPH2RF>
2023-07-12 19:22:04 +0530

    Add project files.

---
+++ Extenssion_Method/Calculation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extenssion_Method
+{
+    public class Calculation
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y) { return x - y; }
+
+    }
+
+
+    public static class ExtensionClass
+    {
+        public static int square(this Calculation c,int x)
+        {
+            return x * x;
+        }
+        public static int multiply(this Calculation c,int x,int y) 
+        {
+            return x * y;
+        }
+
+    }
+}
---
+++ Extenssion_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extenssion_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c=new Calculation();
+            int resullt = c.square(2);
+            Console.WriteLine(resullt);
+            Console.WriteLine(c.multiply(15, 13));
+        }
+    }
+}
